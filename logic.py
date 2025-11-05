e65e29f7c58e1c73750920ee3e3903714e14a6e4
drakeMashburn <drakemashburn8@gmail.com>
2025-11-05 16:34:16 -0600

    Co-authored-by: mattnokes5150 <mattnokes5150@users.noreply.github.com> Co-authored-by: Keean Kruse <KruseKeegan@users.noreply.github.com>

---
+++ logic.py
@@ -0,0 +1,33 @@
+import json
+from gui import GUI
+
+class task:
+    def __init__(self, time, location, description, status):
+        self.time = time #string 
+        self.location = location #string
+        self.description = description #string 
+        self.status = status #boolean
+    def add_task(self, time, location, description, status):
+        self._time = time
+        self._location = location
+        self._description = description
+        self._status = status
+
+
+taskArray = []
+
+
+
+def create_list(taskToAdd, taskArray):
+    
+    
+    
+def remove_list():
+  
+
+    
+def remove_task():
+    
+def move_task():
+    
+def save()
